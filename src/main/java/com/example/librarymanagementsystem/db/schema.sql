85942c1795ba06fe8830484bb99722d9b97eb2ca
rose-tetteh <rose.tetteh@amalitech.com>
2024-11-17 23:44:10 +0000

    refactor:modify database schema

--- src/main/java/com/example/librarymanagementsystem/db/schema.sql
+++ src/main/java/com/example/librarymanagementsystem/db/schema.sql
@@ -4,60 +4,74 @@ CREATE TYPE Status AS ENUM ('available', 'pending', 'borrowed', 'overdue');
 
 -- Superclass Table for Library Resources
 CREATE TABLE LibraryResource (
-                                 resource_id VARCHAR(50) PRIMARY KEY,
+                                 resourceId INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                                  title VARCHAR(255) NOT NULL,
-                                 resourceType ResourceType NOT NULL
+                                 resourceType ResourceType NOT NULL,
+                                 numberOfCopies INT
 );
 
 -- Subclass Table for Books, extending LibraryResource
 CREATE TABLE Book (
-                      resource_id VARCHAR(50) PRIMARY KEY,
+                      resourceId INT PRIMARY KEY,
                       genre VARCHAR(50),
                       author VARCHAR(255),
+                      isbn VARCHAR(255),
                       publicationDate DATE,
                       statusOfBookAvailability Status NOT NULL,
-                      FOREIGN KEY (resource_id) REFERENCES LibraryResource(resource_id)
+                      FOREIGN KEY (resourceId) REFERENCES LibraryResource(resourceId)
 );
 
 -- Subclass Table for Journals, extending LibraryResource
 CREATE TABLE Journal (
-                         resource_id VARCHAR(50) PRIMARY KEY,
+                         resourceId INT PRIMARY KEY,
                          genre VARCHAR(50),
                          author VARCHAR(255),
                          publicationDate DATE,
                          statusOfJournalAvailability Status NOT NULL,
                          volumeNumber INT,
-                         FOREIGN KEY (resource_id) REFERENCES LibraryResource(resource_id)
+                         FOREIGN KEY (resourceId) REFERENCES LibraryResource(resourceId)
 );
 
 -- Table for Patrons
 CREATE TABLE Patron (
-                        user_id BIGINT PRIMARY KEY,
+                        patronId INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                        patronLibraryId VARCHAR(255) NOT NULL UNIQUE,
                         username VARCHAR(255) NOT NULL UNIQUE,
                         email VARCHAR(255) NOT NULL UNIQUE,
+                        phoneNumber VARCHAR(255) NOT NULL,
+                        address VARCHAR(255) NOT NULL,
+                        password VARCHAR(255) NOT NULL
+);
+
+-- Table for Librarian
+CREATE TABLE Librarian (
+                        userId INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                        username VARCHAR(255) NOT NULL UNIQUE,
+                        email VARCHAR(255) NOT NULL UNIQUE,
+                        phoneNumber VARCHAR(255) NOT NULL,
                         password VARCHAR(255) NOT NULL
 );
 
 -- Table for Reservations
 CREATE TABLE Reservation (
-                             reservation_id BIGINT PRIMARY KEY,
-                             resource_id VARCHAR(50) NOT NULL,
-                             user_id BIGINT NOT NULL,
+                             reservationId INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                             resourceId INT NOT NULL,
+                             patronLibraryId VARCHAR(255) NOT NULL,
                              reservationStatus Status NOT NULL,
                              reservationDate TIMESTAMP NOT NULL,
                              queuePosition INT NOT NULL,
-                             FOREIGN KEY (resource_id) REFERENCES LibraryResource(resource_id),
-                             FOREIGN KEY (user_id) REFERENCES Patron(user_id)
+                             FOREIGN KEY (resourceId) REFERENCES LibraryResource(resourceId),
+                             FOREIGN KEY (patronLibraryId) REFERENCES Patron(patronLibraryId)
 );
 
 -- Table for Transactions
 CREATE TABLE Transaction (
-                             transaction_id BIGINT PRIMARY KEY,
-                             resource_id VARCHAR(50) NOT NULL,
-                             user_id BIGINT NOT NULL,
+                             transactionId INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                             resourceId INT NOT NULL,
+                             patronLibraryId VARCHAR(255) NOT NULL,
                              transactionStatus Status NOT NULL,
                              transactionDate TIMESTAMP NOT NULL,
                              dueDate TIMESTAMP NOT NULL,
-                             FOREIGN KEY (resource_id) REFERENCES LibraryResource(resource_id),
-                             FOREIGN KEY (user_id) REFERENCES Patron(user_id)
+                             FOREIGN KEY (resourceId) REFERENCES LibraryResource(resourceId),
+                             FOREIGN KEY (patronLibraryId) REFERENCES Patron(patronLibraryId)
 );
