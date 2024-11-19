0730f804df3a6a69d0a21038fbe1049259c3a3b2
rose-tetteh <rose.tetteh@amalitech.com>
2024-11-19 08:11:08 +0000

    feat:add journal service class

---
+++ src/main/java/com/example/librarymanagementsystem/service/JournalService.java
@@ -0,0 +1,31 @@
+package com.example.librarymanagementsystem.service;
+
+import com.example.librarymanagementsystem.daos.JournalDAO;
+import com.example.librarymanagementsystem.model.Journal;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class JournalService {
+    private JournalDAO journalDAO;
+
+    public List<Journal> getAllJournals() {
+        return journalDAO.getAllResources();
+    }
+
+    public void addJournal(Journal journal) {
+        journalDAO.addResource(journal);
+    }
+
+    public void updateJournal(Journal journal) {
+        journalDAO.updateResource(journal);
+    }
+
+    public void deleteJournal(int resourceId) {
+        journalDAO.deleteResource(resourceId);
+    }
+
+    public Journal getJournalById(int resourceId) throws SQLException {
+        return journalDAO.getResourceById(resourceId);
+    }
+}
