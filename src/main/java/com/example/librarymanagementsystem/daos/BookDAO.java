ea0ba1add12b4ce325c237b36cf6eb8a037671fa
rose-tetteh <rose.tetteh@amalitech.com>
2024-11-19 13:39:51 +0000

    feat:add libraryResourceDAO implementation

--- src/main/java/com/example/librarymanagementsystem/daos/BookDAO.java
+++ src/main/java/com/example/librarymanagementsystem/daos/BookDAO.java
@@ -1,146 +1,83 @@
 package com.example.librarymanagementsystem.daos;
+
 import com.example.librarymanagementsystem.enums.Status;
 import com.example.librarymanagementsystem.model.Book;
-import com.example.librarymanagementsystem.utils.DatabaseConnection;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
 
-/**
- * query1 = For queries where we are inserting values into different tables in one action
- * The type Book dao.
- */
-public class BookDAO implements LibraryResourceDAO<Book> {
-    private String query;
-    private String query1;
-    private ResultSet resultSet;
-    private final Connection connection = DatabaseConnection.getConnection();
-    private PreparedStatement preparedStatement;
+public class BookDAO extends LibraryResourceDAOImpl<Book> {
 
-
-    public List<Book> getAllResources(){
-        List<Book> books = new ArrayList<>();
-        query = """
+    @Override
+    protected String getSelectAllQuery() {
+        return """
                 SELECT lr.resourceId, lr.title, lr.numberOfCopies, b.author, b.genre, b.isbn, b.publicationDate, b.statusOfBookAvailability
                 FROM Book b
                 JOIN LibraryResource lr ON b.resourceId = lr.resourceId
                 """;
-
-        try(Statement statement = connection.createStatement()){
-            resultSet = statement.executeQuery(query);
-                while(resultSet.next()){
-                    books.add(mapResultSetToBook(resultSet));
-                }
-            } catch (SQLException e) {
-            throw new RuntimeException(e.getMessage());
-        }
-        return books;
-    }
-
-    public void addResource(Book book){
-        query = "INSERT INTO LibraryResource (title, resourceType, numberOfCopies) VALUES (?, ?, ?) RETURNING resourceId";
-        query1 = "INSERT INTO Book (resourceId, author, genre, isbn, publicationDate, statusOfBookAvailability) VALUES (?, ?, ?, ?, ?, ?)";
-
-        try {
-            connection.setAutoCommit(false);
-
-            // Insert into LibraryResource
-            preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1, book.getTitle());
-            preparedStatement.setObject(2, "book", Types.OTHER);
-            preparedStatement.setInt(3, book.getNumberOfCopies());
-            resultSet = preparedStatement.executeQuery();
-
-            // Insert into Book
-            if (resultSet.next()){
-                int resourceId = resultSet.getInt("resourceId");
-
-                preparedStatement = connection.prepareStatement(query1);
-                preparedStatement.setInt(1, resourceId);
-                preparedStatement.setString(2, book.getAuthor());
-                preparedStatement.setString(3, book.getGenre());
-                preparedStatement.setString(4, book.getIsbn());
-                preparedStatement.setDate(5, book.getPublicationDate());
-                preparedStatement.setObject(6, "available", Types.OTHER);
-                preparedStatement.executeUpdate();
-            }
-
-            connection.commit();
-        } catch (SQLException e) {
-            throw new RuntimeException(e.getMessage());
-        }
-    }
-
-    public void updateResource(Book book){
-        query = "UPDATE LibraryResource SET title = ?, numberOfCopies = ? WHERE resourceId = ?";
-        query1 = "UPDATE Book SET author = ?, genre = ?, isbn = ?, publicationDate = ?, statusOfBookAvailability = ? WHERE resourceId = ?";
-
-        try{
-            connection.setAutoCommit(false);
-
-            preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1, book.getTitle());
-            preparedStatement.setInt(2, book.getNumberOfCopies());
-            preparedStatement.setInt(3, book.getResourceId());
-            preparedStatement.executeUpdate();
-
-            preparedStatement = connection.prepareStatement(query1);
-            preparedStatement.setString(1, book.getAuthor());
-            preparedStatement.setString(2, book.getGenre());
-            preparedStatement.setString(3, book.getIsbn());
-            preparedStatement.setDate(4, book.getPublicationDate());
-            preparedStatement.setObject(5, "available", Types.OTHER);
-            preparedStatement.setInt(6, book.getResourceId());
-            preparedStatement.executeUpdate();
-
-            connection.commit();
-        } catch (SQLException e){
-            throw new RuntimeException(e.getMessage());
-        }
-    }
-
-    public void deleteResource(int resourceId){
-        query = "DELETE FROM Book WHERE resourceId = ?";
-        query1 = "DELETE FROM LibraryResource WHERE resourceId = ?";
-
-        try{
-            preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1, resourceId);
-            preparedStatement.executeUpdate();
-
-            preparedStatement = connection.prepareStatement(query1);
-            preparedStatement.setInt(1, resourceId);
-            preparedStatement.executeUpdate();
-
-        } catch (Exception e) {
-            throw new RuntimeException("Book with Id: " + resourceId +" does not exist." + e.getMessage());
-        }
     }
 
     @Override
-    public Book getResourceById(int resourceId){
-        query = """
+    protected String getInsertLibraryResourceQuery() {
+        return "INSERT INTO LibraryResource (title, resourceType, numberOfCopies) VALUES (?, ?, ?)";
+    }
+
+    @Override
+    protected String getInsertSpecificResourceQuery() {
+        return "INSERT INTO Book (resourceId, author, genre, isbn, publicationDate, statusOfBookAvailability) VALUES (?, ?, ?, ?, ?, ?)";
+    }
+
+    @Override
+    protected String getUpdateLibraryResourceQuery() {
+        return "UPDATE LibraryResource SET title = ?, numberOfCopies = ? WHERE resourceId = ?";
+    }
+
+    @Override
+    protected String getUpdateSpecificResourceQuery() {
+        return "UPDATE Book SET author = ?, genre = ?, isbn = ?, publicationDate = ?, statusOfBookAvailability = ? WHERE resourceId = ?";
+    }
+
+    @Override
+    protected String getDeleteLibraryResourceQuery() {
+        return "DELETE FROM LibraryResource WHERE resourceId = ?";
+    }
+
+    @Override
+    protected String getDeleteSpecificResourceQuery() {
+        return "DELETE FROM Book WHERE resourceId = ?";
+    }
+
+    @Override
+    protected String getSelectResourceByIdQuery() {
+        return """
                 SELECT lr.resourceId, lr.title, lr.numberOfCopies, b.author, b.genre, b.isbn, b.publicationDate, b.statusOfBookAvailability
                 FROM Book b
                 JOIN LibraryResource lr ON b.resourceId = lr.resourceId
                 WHERE lr.resourceId = ?
                 """;
-        Book book = null;
-        try {
-            preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1, resourceId);
-            resultSet = preparedStatement.executeQuery();
-            if(resultSet.next()){
-                book = mapResultSetToBook(resultSet);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException("Resource not found" + e.getMessage());
-        }
-        return book;
     }
 
-    private Book mapResultSetToBook(ResultSet resultSet) throws SQLException{
+    @Override
+    protected void setLibraryResourceFields(PreparedStatement preparedStatement, Book book) throws SQLException {
+        preparedStatement.setString(1, book.getTitle());
+        preparedStatement.setObject(2, "book", Types.OTHER);
+        preparedStatement.setInt(3, book.getNumberOfCopies());
+    }
+
+    @Override
+    protected void setSpecificResourceFields(PreparedStatement preparedStatement, Book book, int resourceId) throws SQLException {
+        preparedStatement.setInt(1, resourceId);
+        preparedStatement.setString(2, book.getAuthor());
+        preparedStatement.setString(3, book.getGenre());
+        preparedStatement.setString(4, book.getIsbn());
+        preparedStatement.setDate(5, book.getPublicationDate());
+        preparedStatement.setObject(6, book.getStatusOfBookAvailability().name(), Types.OTHER);
+    }
+
+    @Override
+    protected Book mapResultSetToResource(ResultSet resultSet) throws SQLException {
         return new Book.BookBuilder()
                 .resourceId(resultSet.getInt("resourceId"))
                 .title(resultSet.getString("title"))
--- src/main/java/com/example/librarymanagementsystem/daos/JournalDAO.java
+++ src/main/java/com/example/librarymanagementsystem/daos/JournalDAO.java
@@ -2,146 +2,82 @@ package com.example.librarymanagementsystem.daos;
 
 import com.example.librarymanagementsystem.enums.Status;
 import com.example.librarymanagementsystem.model.Journal;
-import com.example.librarymanagementsystem.utils.DatabaseConnection;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
 
-public class JournalDAO implements LibraryResourceDAO<Journal> {
-    private String query;
-    private String query1;
-    private ResultSet resultSet;
-    private final Connection connection = DatabaseConnection.getConnection();
-    private PreparedStatement preparedStatement;
+public class JournalDAO extends LibraryResourceDAOImpl<Journal> {
 
-    public List<Journal> getAllResources(){
-        List<Journal> allJournals = new ArrayList<>();
-        query = """
+    @Override
+    protected String getSelectAllQuery() {
+        return """
                 SELECT lr.resourceId, lr.title, lr.numberOfCopies, j.author, j.genre, j.volumeNumber, j.publicationDate, j.statusOfJournalAvailability
-                FROM Journal b
+                FROM Journal j
                 JOIN LibraryResource lr ON j.resourceId = lr.resourceId
                 """;
-
-        try(Statement statement = connection.createStatement()){
-            resultSet = statement.executeQuery(query);
-            while(resultSet.next()){
-               allJournals.add(mapResultSetToJournal(resultSet));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e.getMessage());
-        }
-        return allJournals;
     }
 
     @Override
-    public void addResource(Journal journal)  {
-        query = "INSERT INTO LibraryResource (title, resourceType, numberOfCopies) VALUES (?, ?, ?) RETURNING resourceId";
-        query1 = "INSERT INTO Journal (resourceId, author, genre, volumeNumber, publicationDate, statusOfJournalAvailability) VALUES (?, ?, ?, ?, ?, ?)";
-
-        try {
-            connection.setAutoCommit(false);
-
-            // Insert into LibraryResource
-            preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1, journal.getTitle());
-            preparedStatement.setObject(2, "journal", Types.OTHER);
-            preparedStatement.setInt(3, journal.getNumberOfCopies());
-            resultSet = preparedStatement.executeQuery();
-
-            // Insert into Journal
-            if (resultSet.next()){
-                int resourceId = resultSet.getInt("resourceId");
-
-                preparedStatement = connection.prepareStatement(query1);
-                preparedStatement.setInt(1, resourceId);
-                preparedStatement.setString(2, journal.getAuthor());
-                preparedStatement.setString(3, journal.getGenre());
-                preparedStatement.setInt(4, journal.getVolumeNumber());
-                preparedStatement.setDate(5, journal.getPublicationDate());
-                preparedStatement.setObject(6, "available", Types.OTHER);
-                preparedStatement.executeUpdate();
-            }
-
-            connection.commit();
-        } catch (SQLException e) {
-            throw new RuntimeException(e.getMessage());
-        }
-
+    protected String getInsertLibraryResourceQuery() {
+        return "INSERT INTO LibraryResource (title, resourceType, numberOfCopies) VALUES (?, ?, ?) RETURNING resourceId";
     }
 
     @Override
-    public void updateResource(Journal journal){
-        query = "UPDATE LibraryResource SET title = ?, numberOfCopies = ? WHERE resourceId = ?";
-        query1 = "UPDATE Journal SET author = ?, genre = ?, volumeNumber = ?, publicationDate = ?, statusOfJournalAvailability = ? WHERE resourceId = ?";
-
-        try{
-            connection.setAutoCommit(false);
-
-            preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1, journal.getTitle());
-            preparedStatement.setInt(2, journal.getNumberOfCopies());
-            preparedStatement.setInt(3, journal.getResourceId());
-            preparedStatement.executeUpdate();
-
-            preparedStatement = connection.prepareStatement(query1);
-            preparedStatement.setString(1, journal.getAuthor());
-            preparedStatement.setString(2, journal.getGenre());
-            preparedStatement.setInt(3, journal.getVolumeNumber());
-            preparedStatement.setDate(4, journal.getPublicationDate());
-            preparedStatement.setObject(5, "available", Types.OTHER);
-            preparedStatement.setInt(6, journal.getResourceId());
-            preparedStatement.executeUpdate();
-
-            connection.commit();
-        } catch (SQLException e){
-            throw new RuntimeException(e.getMessage());
-        }
-
+    protected String getInsertSpecificResourceQuery() {
+        return "INSERT INTO Journal (resourceId, author, genre, volumeNumber, publicationDate, statusOfJournalAvailability) VALUES (?, ?, ?, ?, ?, ?)";
     }
 
     @Override
-    public void deleteResource(int resourceId)  {
-        query = "DELETE FROM Journal WHERE resourceId = ?";
-        query1 = "DELETE FROM LibraryResource WHERE resourceId = ?";
-
-        try{
-            preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1, resourceId);
-            preparedStatement.executeUpdate();
-
-            preparedStatement = connection.prepareStatement(query1);
-            preparedStatement.setInt(1, resourceId);
-            preparedStatement.executeUpdate();
-
-        } catch (Exception e) {
-            throw new RuntimeException("Journal with Id: " + resourceId +" does not exist." + e.getMessage());
-        }
+    protected String getUpdateLibraryResourceQuery() {
+        return "UPDATE LibraryResource SET title = ?, numberOfCopies = ? WHERE resourceId = ?";
     }
 
     @Override
-    public Journal getResourceById(int resourceId)  {
-        query = """
+    protected String getUpdateSpecificResourceQuery() {
+        return "UPDATE Journal SET author = ?, genre = ?, volumeNumber = ?, publicationDate = ?, statusOfJournalAvailability = ? WHERE resourceId = ?";
+    }
+
+    @Override
+    protected String getDeleteLibraryResourceQuery() {
+        return "DELETE FROM LibraryResource WHERE resourceId = ?";
+    }
+
+    @Override
+    protected String getDeleteSpecificResourceQuery() {
+        return "DELETE FROM Journal WHERE resourceId = ?";
+    }
+
+    @Override
+    protected String getSelectResourceByIdQuery() {
+        return """
                 SELECT lr.resourceId, lr.title, lr.numberOfCopies, j.author, j.genre, j.volumeNumber, j.publicationDate, j.statusOfJournalAvailability
-                FROM Journal b
+                FROM Journal j
                 JOIN LibraryResource lr ON j.resourceId = lr.resourceId
                 WHERE lr.resourceId = ?
                 """;
-        Journal journal = null;
-        try {
-            preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setInt(1, resourceId);
-            resultSet = preparedStatement.executeQuery();
-            if(resultSet.next()){
-                journal = mapResultSetToJournal(resultSet);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException("Resource not found" + e.getMessage());
-        }
-        return journal;
     }
 
-    private Journal mapResultSetToJournal(ResultSet resultSet) throws SQLException{
+    @Override
+    protected void setLibraryResourceFields(PreparedStatement preparedStatement, Journal resource) throws SQLException {
+        preparedStatement.setString(1, resource.getTitle());
+        preparedStatement.setObject(2, "journal", Types.OTHER);
+        preparedStatement.setInt(3, resource.getNumberOfCopies());
+    }
+
+    @Override
+    protected void setSpecificResourceFields(PreparedStatement preparedStatement, Journal resource, int resourceId) throws SQLException {
+        preparedStatement.setInt(1, resourceId);
+        preparedStatement.setString(2, resource.getAuthor());
+        preparedStatement.setString(3, resource.getGenre());
+        preparedStatement.setInt(4, resource.getVolumeNumber());
+        preparedStatement.setDate(5, resource.getPublicationDate());
+        preparedStatement.setObject(6, resource.getStatusOfJournalAvailability().name(), Types.OTHER);
+    }
+
+    @Override
+    protected Journal mapResultSetToResource(ResultSet resultSet) throws SQLException {
         return new Journal.JournalBuilder()
                 .resourceId(resultSet.getInt("resourceId"))
                 .title(resultSet.getString("title"))
